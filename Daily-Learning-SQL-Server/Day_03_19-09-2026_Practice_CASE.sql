fc1146d91a930a95eafbacb1b7bf156d232d07ef
Vaibhav Ghatmal <175679884+VaibhavG808@users.noreply.github.com>
2026-09-19 18:29:52 +0530

    Add files via upload

---
+++ Daily-Learning-SQL-Server/Day_03_19-09-2026_Practice_CASE.sql
@@ -0,0 +1,83 @@
+-- SECTION 5 --
+-- AGGRIGATE FUNCTIONS --
+
+USE bank_db;
+
+SELECT * FROM Employees;
+
+/*
+Q.1 How to find total no. of employees?
+Q.2 Employee with Max or Min salary
+Q.3 Average Salary of employees
+Q.4 Sum/total salary paid
+*/
+
+-- Aggrigate Functions --
+
+-- COUNT --
+SELECT COUNT(EMP_ID) FROM Employees;
+
+-- MIN --
+SELECT MIN (SALARY) FROM Employees;
+
+-- MAX --
+SELECT MAX (SALARY) FROM Employees;
+
+-- AVG --
+SELECT AVG(SALARY) FROM Employees;
+
+-- SUM --
+SELECT SUM (SALARY) FROM Employees;
+
+
+
+-- SECTION 5.2 --
+
+-- GROUP BY --
+
+-- Q.1 No. of employees by department --
+SELECT department, COUNT(EMP_ID) AS EMPCOUNT FROM Employees GROUP BY department;
+
+/*
+Q.1 Find number of employees in each department
+Q.2 Find number of employees in each city
+Q.3 Find average salary in each department
+*/
+
+SELECT DEPARTMENT, COUNT(EMP_ID) FROM Employees GROUP BY department;
+
+SELECT CITY, COUNT(EMP_ID) AS total_employee FROM EMPLOYEES GROUP BY CITY;
+
+SELECT DEPARTMENT, AVG(SALARY) Average_salary FROM Employees GROUP BY department;
+
+SELECT DEPARTMENT, SUM(SALARY) AS TOTAL_SALARY FROM Employees GROUP BY department;
+
+SELECT job_title, COUNT(emp_id) AS total_emp FROM Employees GROUP BY job_title;
+
+
+-- MULTI COLUMN GROUPING --
+SELECT department, city, COUNT(emp_id) total_emp
+FROM Employees GROUP BY department, city
+ORDER BY department;
+
+
+-- HAVING CLAUSE --
+/*
+1. Find departments with more than 2 employees
+2. Find job titles with an average salary above 40000
+3. Find department with average salary above 50000
+*/
+
+SELECT department, COUNT(emp_id) AS emp
+FROM Employees GROUP BY department
+HAVING COUNT(emp_id) > 1;
+
+
+SELECT job_title, AVG(salary) avg_salary 
+FROM Employees GROUP BY job_title 
+HAVING AVG(salary) > 40000;
+
+
+SELECT department, AVG(salary) avg_salary 
+FROM Employees GROUP BY department
+HAVING AVG(salary) > 50000;
